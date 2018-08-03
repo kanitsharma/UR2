2f6d3106fb5641580404016ca670a98cb6a8bbfe
kanitsharma <kanitshrm09@gmail.com>
2018-08-04 03:04:44 +0530

    removed constants

--- packages/ur2/src/actions.js
+++ packages/ur2/src/actions.js
@@ -1,38 +1,30 @@
 import { parse } from 'query-string'
-import {
-  PUSH,
-  REPLACE,
-  GO,
-  GO_BACK,
-  GO_FORWARD,
-  LOCATION_CHANGE
-} from './constants'
 
 export const push = href => ({
-  type: PUSH,
+  type: 'ROUTER/PUSH',
   payload: href
 })
 
 export const replace = href => ({
-  type: REPLACE,
+  type: 'ROUTER/REPLACE',
   payload: href
 })
 
 export const go = index => ({
-  type: GO,
+  type: 'ROUTER/GO',
   payload: index
 })
 
 export const goBack = () => ({
-  type: GO_BACK
+  type: 'ROUTER/GO_BACK'
 })
 
 export const goForward = () => ({
-  type: GO_FORWARD
+  type: 'ROUTER/GO_FORWARD'
 })
 
 export const locationChange = ({ pathname, search, hash }) => ({
-  type: LOCATION_CHANGE,
+  type: 'ROUTER/LOCATION_CHANGE',
   payload: {
     pathname,
     search,
